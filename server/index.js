effc95651a265ffa8c89ff85279a7edea8020339
Ubuntu <ubuntu@ip-172-31-22-219.us-east-2.compute.internal>
2020-12-15 18:02:31 +0000

    modified

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = 5000;
+
+console.log(PORT);
+
+const router = require('./router');
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
