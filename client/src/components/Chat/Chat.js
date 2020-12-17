4811b230878f642d23fb63818b523fdf0bf135d3
Qiyu Wang <wangqy1216@gamil.com>
2020-12-17 17:13:56 -0600

    add css

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+import './Chat.css';
+
+let socket;
+
+const Chat = ({ location }) => {
+    const [ name, setName ] = useState('');
+    const [ room, setRoom ] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search)
+    
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        console.log(socket);
+    });
+
+    return (
+        <div>
+            <h1>Chat</h1>    
+        </div>
+    )
+}
+
+export default Chat;
